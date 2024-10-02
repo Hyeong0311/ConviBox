29ff73e2af5b5d2a6f47c9c98ab860a664d09f16
jhyeong <hyeongcode@gmail.com>
2024-10-02 16:17:00 +0900

    re

--- src/api/productAPI.ts
+++ src/api/productAPI.ts
@@ -2,13 +2,20 @@ import axios from "axios";
 import {IProduct, IProducts} from "../types/product.ts";
 
 
-//const host = 'http://1.255.178.102:8089/api/products'
-const host = 'http://localhost:8089/api/products'
+const host = 'http://1.255.178.102:8089/api/products'
 
 
 export const getSearchedList = async (query: string): Promise<IProducts> => {
 
     const res = await axios.get<IProducts>(`${host}/list?keyword=${query}`)
+
+    return res.data
+}
+
+
+export const getOne = async (pno: string | undefined): Promise<IProduct> => {
+
+    const res = await axios.get<IProduct>(`${host}/${pno}`)
 
     return res.data
 }
