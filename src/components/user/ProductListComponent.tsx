a53d8fa31c847ecdc8aa50db270bc9fcf33c9914
yun9699 <cha23215@daum.net>
2024-10-07 17:37:03 +0900

    10원짜리들 수정

--- src/components/user/ProductListComponent.tsx
+++ src/components/user/ProductListComponent.tsx
@@ -1,27 +1,26 @@
-import React, {ReactElement, useEffect, useState} from 'react';
-import {IProduct, IProducts} from "../../types/product.ts";
-import {getList} from "../../api/productAPI.ts";
+import React, { ReactElement, useEffect, useState, useRef } from 'react';
+import { IProduct, IProducts } from "../../types/product.ts";
+import { getList } from "../../api/productAPI.ts";
 import LoadingComponent from "../../common/LoadingComponent.tsx";
-import {useNavigate} from "react-router-dom";
-
+import { useNavigate } from "react-router-dom";
 
 const initialState: IProducts = {
-
     dtoList: []
 }
 
 function ProductListComponent(): ReactElement {
-
     const [products, setProducts] = useState<IProducts>({...initialState});
     const [loading, setLoading] = useState<boolean>(false);
     const navigate = useNavigate();
 
+    const under3000Ref = useRef<HTMLDivElement>(null);
+    const over3000Ref = useRef<HTMLDivElement>(null);
+    const over5000Ref = useRef<HTMLDivElement>(null);
+    const over10000Ref = useRef<HTMLDivElement>(null);
+
     const ProductCard: React.FC<{ product: IProduct }> = ({ product }) => {
-
         const host = 'http://1.255.178.102:8089/api/products/view/';
-
         return (
-
             <div
                 className="min-w-[150px] max-w-[150px] bg-white shadow-lg rounded-lg p-4 min-h-[200px]"
                 onClick={() => productClick(product.pno)}
@@ -46,11 +45,8 @@ function ProductListComponent(): ReactElement {
     };
 
     useEffect(() => {
-
         setLoading(true);
-
         getList().then(item => {
-
             setProducts(item);
             setLoading(false);
         });
@@ -62,7 +58,6 @@ function ProductListComponent(): ReactElement {
     const over10000: IProduct[] = [];
 
     products.dtoList.forEach((product) => {
-
         if (product.price < 3000) {
             under3000.push(product);
         } else if (product.price >= 3000 && product.price < 5000) {
@@ -75,48 +70,111 @@ function ProductListComponent(): ReactElement {
     });
 
     const productClick = (pno: number) => {
-
         navigate(`/recipe/${pno}`);
     };
 
+    // 좌우 버튼 클릭 시 스크롤 이동 함수
+    const scrollLeft = (ref: React.RefObject<HTMLDivElement>) => {
+        if (ref.current) {
+            ref.current.scrollBy({ left: -300, behavior: "smooth" });
+        }
+    };
+
+    const scrollRight = (ref: React.RefObject<HTMLDivElement>) => {
+        if (ref.current) {
+            ref.current.scrollBy({ left: 300, behavior: "smooth" });
+        }
+    };
 
     return (
         <div className="p-4">
-
             {loading && <LoadingComponent></LoadingComponent>}
 
             {!loading && <>
-
-                <h2 className="text-lg font-bold mb-4">3천원 이하</h2>
-                <div className="flex space-x-4 overflow-x-auto pb-4">
-                    {under3000.map((product, index) => (
-                        <ProductCard key={index} product={product}/>
-                    ))}
+                <div className="relative">
+                    <h2 className="text-lg font-bold mb-4">3천원 이하</h2>
+                    <button
+                        onClick={() => scrollLeft(under3000Ref)}
+                        className="absolute left-0 top-1/2 transform -translate-y-1/2 z-10 rounded-full w-12 h-12 flex items-center justify-center"
+                    >
+                        <img src="/icon-park-outline--left-c.svg" alt="left-arrow" className="w-full h-full" />
+                    </button>
+                    <div className="flex space-x-4 overflow-x-auto pb-4" ref={under3000Ref}>
+                        {under3000.map((product, index) => (
+                            <ProductCard key={index} product={product} />
+                        ))}
+                    </div>
+                    <button
+                        onClick={() => scrollRight(under3000Ref)}
+                        className="absolute right-0 top-1/2 transform -translate-y-1/2 z-10 rounded-full w-12 h-12 flex items-center justify-center"
+                    >
+                        <img src="/icon-park-outline--right-c.svg" alt="right-arrow" className="w-full h-full" />
+                    </button>
                 </div>
 
-                <h2 className="text-lg font-bold mb-4">5천원 이하</h2>
-                <div className="flex space-x-4 overflow-x-auto pb-4">
-                    {over3000.map((product, index) => (
-                        <ProductCard key={index} product={product}/>
-                    ))}
+                <div className="relative">
+                    <h2 className="text-lg font-bold mb-4">5천원 이하</h2>
+                    <button
+                        onClick={() => scrollLeft(over3000Ref)}
+                        className="absolute left-0 top-1/2 transform -translate-y-1/2 z-10 rounded-full w-12 h-12 flex items-center justify-center"
+                    >
+                        <img src="/icon-park-outline--left-c.svg" alt="left-arrow" className="w-full h-full" />
+                    </button>
+                    <div className="flex space-x-4 overflow-x-auto pb-4" ref={over3000Ref}>
+                        {over3000.map((product, index) => (
+                            <ProductCard key={index} product={product} />
+                        ))}
+                    </div>
+                    <button
+                        onClick={() => scrollRight(over3000Ref)}
+                        className="absolute right-0 top-1/2 transform -translate-y-1/2 z-10 rounded-full w-12 h-12 flex items-center justify-center"
+                    >
+                        <img src="/icon-park-outline--right-c.svg" alt="right-arrow" className="w-full h-full" />
+                    </button>
                 </div>
 
-                <h2 className="text-lg font-bold mb-4">1만원 이하</h2>
-                <div className="flex space-x-4 overflow-x-auto pb-4">
-                    {over5000.map((product, index) => (
-                        <ProductCard key={index} product={product}/>
-                    ))}
+                <div className="relative">
+                    <h2 className="text-lg font-bold mb-4">1만원 이하</h2>
+                    <button
+                        onClick={() => scrollLeft(over5000Ref)}
+                        className="absolute left-0 top-1/2 transform -translate-y-1/2 z-10 rounded-full w-12 h-12 flex items-center justify-center"
+                    >
+                        <img src="/icon-park-outline--left-c.svg" alt="left-arrow" className="w-full h-full" />
+                    </button>
+                    <div className="flex space-x-4 overflow-x-auto pb-4" ref={over5000Ref}>
+                        {over5000.map((product, index) => (
+                            <ProductCard key={index} product={product} />
+                        ))}
+                    </div>
+                    <button
+                        onClick={() => scrollRight(over5000Ref)}
+                        className="absolute right-0 top-1/2 transform -translate-y-1/2 z-10 rounded-full w-12 h-12 flex items-center justify-center"
+                    >
+                        <img src="/icon-park-outline--right-c.svg" alt="right-arrow" className="w-full h-full" />
+                    </button>
                 </div>
 
-                <h2 className="text-lg font-bold mb-4">1만원 이상</h2>
-                <div className="flex space-x-4 overflow-x-auto pb-4">
-                    {over10000.map((product, index) => (
-                        <ProductCard key={index} product={product}/>
-                    ))}
+                <div className="relative">
+                    <h2 className="text-lg font-bold mb-4">1만원 이상</h2>
+                    <button
+                        onClick={() => scrollLeft(over10000Ref)}
+                        className="absolute left-0 top-1/2 transform -translate-y-1/2 z-10 rounded-full w-12 h-12 flex items-center justify-center"
+                    >
+                        <img src="/icon-park-outline--left-c.svg" alt="left-arrow" className="w-full h-full" />
+                    </button>
+                    <div className="flex space-x-4 overflow-x-auto pb-4" ref={over10000Ref}>
+                        {over10000.map((product, index) => (
+                            <ProductCard key={index} product={product} />
+                        ))}
+                    </div>
+                    <button
+                        onClick={() => scrollRight(over10000Ref)}
+                        className="absolute right-0 top-1/2 transform -translate-y-1/2 z-10 rounded-full w-12 h-12 flex items-center justify-center"
+                    >
+                        <img src="/icon-park-outline--right-c.svg" alt="right-arrow" className="w-full h-full" />
+                    </button>
                 </div>
-
-                </>}
-
+            </>}
         </div>
     );
 }
--- src/components/user/SearchModalComponent.tsx
+++ src/components/user/SearchModalComponent.tsx
@@ -1,50 +1,39 @@
-import {ReactElement, useState} from "react";
-import {useNavigate} from "react-router-dom";
-
+import { ReactElement, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 interface LoadingComponentProps {
-
     onClose: () => void;
 }
 
-
-function SearchModalComponent({onClose}: LoadingComponentProps): ReactElement {
-
+function SearchModalComponent({ onClose }: LoadingComponentProps): ReactElement {
     const [searchKeyword, setSearchKeyword] = useState('');
     const navigate = useNavigate();
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
-
         const value = e.target.value;
-
         setSearchKeyword(value);
-    }
+    };
 
     const handleClick = () => {
-
         const queryStr = `?keyword=${searchKeyword}`;
-
         navigate({
             pathname: '/search',
             search: queryStr
-        })
-        onClose()
-    }
-
+        });
+        onClose();
+    };
 
     return (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-20">
             <div className="bg-white rounded-lg shadow-lg p-6 w-96">
                 <h2 className="text-2xl font-bold mb-4 text-gray-800">검색</h2>
-
                 <input
                     type="text"
                     name="searchItem"
-                    onChange={(e) => handleChange(e)}
+                    onChange={handleChange}
                     placeholder="검색어를 입력하세요"
                     className="w-full p-2 mb-4 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-yellow-400"
                 />
-
                 <div className="flex justify-end space-x-4">
                     <button
                         onClick={handleClick}
